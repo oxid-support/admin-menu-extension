5767636ed504cc54dee995173246a95264a72a60
KiryiMONZTA <dev@kiryi.net>
2023-05-09 11:30:25 +0000

    Improved basic information.

--- metadata.php
+++ metadata.php
@@ -4,26 +4,29 @@
  * Metadata version
  */
 $sMetadataVersion = '2.1';
+
 /**
  * Module information
  */
 $aModule = [
     'id' => 'oxsadminmenuext',
-    'title' => 'Adds a new admin page with tabs.',
-    'description' => '',
+    'title' => 'OXID Support - Example Admin Page',
+    'description' => 'Adds a new admin page with tabs.',
     'thumbnail' => '',
-    'version' => '1.0',
+    'version' => '1.0.0',
     'author' => 'OXID Support',
+    'url' => 'https://www.oxid-esales.com/',
+    'email' => 'support@oxid-esales.com',
     'controllers' => [
-        'oxsadminmenuext_example'    => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\Example::class,
-        'oxsadminmenuext_list'       => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleList::class,
-        'oxsadminmenuext_main'       => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleMain::class,
-        'oxsadminmenuext_extends'    => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleExtends::class,
+        'oxsadminmenuext_example' => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\Example::class,
+        'oxsadminmenuext_list'    => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleList::class,
+        'oxsadminmenuext_main'    => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleMain::class,
+        'oxsadminmenuext_extends' => \OxidSupport\AdminMenuExtension\Application\Controller\Admin\ExampleExtends::class,
     ],
     'templates' => [
-        'example.tpl'               => 'oxs/adminmenuext/Application/views/admin/example.tpl',
-        'example_list.tpl'          => 'oxs/adminmenuext/Application/views/admin/example_list.tpl',
-        'example_main.tpl'          => 'oxs/adminmenuext/Application/views/admin/example_main.tpl',
-        'example_extends.tpl'       => 'oxs/adminmenuext/Application/views/admin/example_extends.tpl',
+        'example.tpl'         => 'oxs/adminmenuext/Application/views/admin/example.tpl',
+        'example_list.tpl'    => 'oxs/adminmenuext/Application/views/admin/example_list.tpl',
+        'example_main.tpl'    => 'oxs/adminmenuext/Application/views/admin/example_main.tpl',
+        'example_extends.tpl' => 'oxs/adminmenuext/Application/views/admin/example_extends.tpl',
     ],
 ];
