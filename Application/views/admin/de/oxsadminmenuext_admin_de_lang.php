fe0e3051d4ada7bd7396921bd4d1287e829c1603
KiryiMONZTA <dev@kiryi.net>
2023-05-09 12:10:15 +0000

    Added translations.

---
+++ Application/views/admin/de/oxsadminmenuext_admin_de_lang.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+$sLangName = 'Deutsch';
+
+$aLang = [
+    'charset' => 'UTF-8',
+
+    'oxsexample_menu' => 'Beispiel',
+    'oxsexample_submenu_example' => 'Beispiel mit Tabs',
+    'oxsexample_tab_main' => 'Stamm',
+    'oxsexample_tab_extends' => 'Erweitert',
+];
---
+++ Application/views/admin/en/oxsadminmenuext_admin_de_lang.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+$sLangName = 'English';
+
+$aLang = [
+    'charset' => 'UTF-8',
+
+    'oxsexample_menu' => 'Example',
+    'oxsexample_submenu_example' => 'Example with Tabs',
+    'oxsexample_tab_main' => 'Main',
+    'oxsexample_tab_extends' => 'Extended',
+];
